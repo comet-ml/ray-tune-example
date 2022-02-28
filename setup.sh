f0d050618f4719c86a665f5676ce77d1994202ed
DN6 <dhruv.nair@gmail.com>
2022-02-28 18:03:06 +0530

    update setup script to use requirements file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 pip install -U https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-2.0.0.dev0-cp38-cp38-manylinux2014_x86_64.whl
-pip install comet_ml
-pip install pandas
-pip install tabulate
+pip install -r requirements.txt
 
 git clone https://github.com/ray-project/ray
 cd ray
