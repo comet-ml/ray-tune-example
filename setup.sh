a846bb57134d491013ca5a858e952ae7e86c3c22
Dhruv Nair <dhruv.nair@gmail.com>
2022-03-31 13:53:53 +0000

    remove install ray from wheels

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-pip install -U https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-2.0.0.dev0-cp38-cp38-manylinux2014_x86_64.whl
 pip install comet_ml
 pip install pandas
 pip install tabulate
